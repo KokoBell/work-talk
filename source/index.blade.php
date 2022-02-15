9ea8e84a8ac5cb64726c283f0f8e17eeece596ae
KokoBell <57895838+KokoBell@users.noreply.github.com>
2022-02-15 18:47:32 +0200

    Updating home page to include posts

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,4 +2,19 @@
 
 @section('content')
     <h1>Welcome to Work Talk</h1>
+
+    @section('title', 'Posts')
+
+    <ul>
+        @forelse ($posts->sortBy('title') as $post)
+            <li>
+                <a href="{{ $post->getPath() }}">{{ $post->title }}</a>
+                <small>{{ $post->prettyDate() }}</small>
+            </li>
+        @empty
+            <p>No posts to show.</p>
+        @endforelse
+    </ul>
+@endsection
+
 @endsection
